98c4b3befc0d462fa0f598debe6ece7ad25c31c3
Sergio Novoa <sergio.novoa1109@gmail.com>
2024-04-11 23:49:27 -0500

    preparar mongobd y envs

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,17 +1,27 @@
 import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { InjectModel } from '@nestjs/mongoose';
+
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { Model, isValidObjectId } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
-import { InjectModel } from '@nestjs/mongoose';
+import { PaginationDto } from 'src/common/dto/pagination.dto';
 
 @Injectable()
 export class PokemonService {
 
+  private defaultLimit: number;
+
   constructor(
     @InjectModel( Pokemon.name )
-    private readonly pokemonModel: Model<Pokemon>
-  ){}
+    private readonly pokemonModel: Model<Pokemon>,
+
+    private readonly configService: ConfigService,
+  ){
+    this.defaultLimit = this.configService.get<number>('defaultLimit')
+    // console.log({var: +process.env.PORT})
+  }
 
   async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLocaleLowerCase();
@@ -25,8 +35,15 @@ export class PokemonService {
       
   }
 
-  findAll() {
-    return `This action returns all pokemon`;
+  findAll(paginationDto: PaginationDto) {
+    const {limit = this.defaultLimit, offSet = 0} = paginationDto
+    return this.pokemonModel.find()
+    .limit(limit)
+    .skip(offSet)
+    .sort({
+      no: 1
+    })
+    .select('-__v');
   }
 
   async findOne(term: string) {
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -2,24 +2,42 @@ import { Injectable } from '@nestjs/common';
 
 import axios, { AxiosInstance } from 'axios'
 import { PokeResponse } from './interfaces/poke-response.interface';
+import { InjectModel } from '@nestjs/mongoose';
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
- 
-  private readonly axios:  AxiosInstance = axios;
 
-  async executeSeed(){
-    
-    const {data} = await this.axios.get<PokeResponse> ('https://pokeapi.co/api/v2/pokemon?limit=10')
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel: Model<Pokemon>,
 
-    data.results.forEach(({ name, url }) =>{
+    private readonly http: AxiosAdapter
+  ){}
+
+  async executeSeed() {
+
+    await this.pokemonModel.deleteMany({}); // delete * from pokemons;
+
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+
+    const pokemonToInsert: { name: string, no: number }[] = [];
+
+    data.results.forEach(({ name, url }) => {
 
       const segments = url.split('/');
+      const no = +segments[ segments.length - 2 ];
 
-      const no = +segments [segments.length - 2]
+      // const pokemon = await this.pokemonModel.create({ name, no });
+      pokemonToInsert.push({ name, no }); // [{ name: bulbasaur, no: 1 }]
 
-      console.log({name, no})
-    })
-    return data.results;
+    });
+
+    await this.pokemonModel.insertMany(pokemonToInsert);
+
+
+    return 'Seed Executed';
   }
 }
